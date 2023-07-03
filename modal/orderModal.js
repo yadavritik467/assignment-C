34ced48f1ac5a04d6319d2b23e459e9306ab8671
Ritik Yadav <130385697+yadavritik467@users.noreply.github.com>
2023-07-03 19:30:55 +0530

    initial commit

--- modal/orderModal.js
+++ modal/orderModal.js
@@ -4,10 +4,7 @@ const orderSchema = new mongoose.Schema({
     orderId: { type: String, required: true },
     title: { type: String, required: true },
     description: { type: String, required: true },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-      },
+    createdAt: { type: Date, default: Date.now},
 })
 
 const Order = mongoose.model("Order",orderSchema)
